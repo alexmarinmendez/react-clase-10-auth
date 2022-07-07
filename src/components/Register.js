31b39ba022f634a309d2a516b52a3ff164616e7a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-06 19:26:36 -0500

    feat: add form for Register.js component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    // console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" placeholder="youremail@company.com" onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="**************" onChange={handleChange} />
+        <button>Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
