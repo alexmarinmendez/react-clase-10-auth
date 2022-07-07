05b5952f201ac5f101e7f6d20aec15dab88d5c06
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-06 20:18:14 -0500

    feat: login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+  const { error, login } = useAuth();
+
+  const handleChange = ({ target: { name, value } }) => {
+    // console.log(name, value);
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(user.email, user.password);
+  };
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" placeholder="youremail@company.com" onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="**************" onChange={handleChange} />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
